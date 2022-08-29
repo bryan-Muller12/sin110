d2ab6daf1d0274a9a6096ddb8f021d67cea0a98d
Bryan Muller <brysn.ms@gmail.com>
2022-08-29 15:23:50 -0300

    código para leitura de arquivo e calculo de sua dimensão comentado

--- ATV1/main.py
+++ ATV1/main.py
@@ -1,26 +1,38 @@
 import numpy as np
 
-#Fução para abrir o ar
-def criaMatrizPeca(inst): 
-    caminho = 'C:/Users/bryan/OneDrive/Documentos/SIN110/ATV1/arquivos_matriz/' + inst + '.txt'
+#Fução para crair uma matriz através da leitura de um arquivo que é passada como instancia seu retorno é a matriz
+def criaMatriz(inst): 
+    caminho = 'C:/Users/bryan/OneDrive/Área de Trabalho/sin110/ATV1/arquivos_matriz/' + inst + '.txt'
     with open(caminho, 'rb') as f:
+        #ncols lê quantas colunas tem a matriz e a variavel data traz a primeira linha da matriz
         ncols = [int(field) for field in f.readline().split()] 
         data = np.genfromtxt(f, dtype='int32', max_rows=len(ncols))
     matrix = np.vstack((ncols,data))
     return matrix
 
+#Função para salvar em arquivo de texto o nome do arquivo lido e a dimensão da matriz 
 def salvaResultado(resultado,nome):
-    arquivo = open('C:/Users/bryan/OneDrive/Documentos/SIN110/ATV1/arquivos_matriz/Resultados/' + nome + '.txt', "w")
+    arquivo = open('C:/Users/bryan/OneDrive/Área de Trabalho/sin110/ATV1/arquivos_matriz/Resultados/' + nome + '.txt', "w")
     arquivo.writelines(resultado + "\n")
     arquivo.close()
 
-arquivoEntrada = "ponte"
-a = criaMatrizPeca(arquivoEntrada)
-print(a)
-print(a.shape)
+#Variavel arquivoEntrada modifica qual arquivo sera aberto
+arquivoEntrada = "zachary"
 
+#chama função criaMatriz e armazena em uma variavel
+a = criaMatriz(arquivoEntrada)
+print(a)
+
+#shape é usado para obter as dimensões da matriz
+print("Dimensão: ", a.shape)
+
+#Variavel resultado é uma string que armazena o que sera inscrito no arquivo resultado
 resultado = '\nNome: ' + arquivoEntrada + '\nNumeros de linhas: ' + str(a.shape[0]) + '\nNumeros de colunas: ' + str(a.shape[1])
+
+#Variavel para dar nome ao arquivo resultante
 nome = arquivoEntrada + 'Resultado'
+
+#chama função para salvar
 salvaResultado(resultado,nome)
 
 
